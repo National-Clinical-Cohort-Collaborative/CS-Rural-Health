27566aee8fa77cee0449db488c819c5e81736dde
Geneva Marshall <geneva-marshall@ouhsc.edu>
2021-08-24 08:46:12 -0500

    revise format to new codesets and omit RXNorm Extension concept_ids.
    @wibeasley

---
+++ steroid-background/concept-sets/input/wildcard-queries-inhaled-nasal-spray.sql
@@ -0,0 +1,61 @@
+  use cdw_omop_1
+  SELECT
+      --csm.codeset
+      c.concept_id
+      ,c.concept_name
+      ,c.standard_concept
+      ,'' as standard_concept_caption
+      ,c.invalid_reason
+      ,'' as invalid_reason_caption
+      ,c.concept_code
+      ,c.domain_id
+      ,c.vocabulary_id
+      ,c.concept_class_id
+    FROM  v6.concept c
+    WHERE 
+    domain_id = 'drug'
+    and
+    vocabulary_id != 'RXNorm Extension'
+    and 
+    (concept_name like '%fluticasone%'
+    or
+    concept_name like '%Ciclesonide%'
+    or
+    concept_name like '%Mometasone%'
+    or
+    concept_name like '%Budesonide%'
+    or
+    concept_name like '%Beclomethasone%'
+    )
+
+    SELECT
+      --csm.codeset
+      c.concept_id
+      ,c.concept_name
+      ,c.standard_concept
+      ,'' as standard_concept_caption
+      ,c.invalid_reason
+      ,'' as invalid_reason_caption
+      ,c.concept_code
+      ,c.domain_id
+      ,c.vocabulary_id
+      ,c.concept_class_id
+    FROM  v6.concept c
+    WHERE 
+    domain_id = 'drug'
+    and
+    vocabulary_id != 'RXNorm Extension'
+    and 
+    (concept_name like '%Budesonide%'
+    or
+    concept_name like '%Fluticasone%'
+    or
+    concept_name like '%Ciclesonide%'
+    or
+    concept_name like '%Triamcinolone%' 
+    or
+    concept_name like '%Mometasone%'
+    or
+    concept_name like '%Beclometasone%' 
+    )
+
