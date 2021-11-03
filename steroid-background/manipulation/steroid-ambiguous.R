c38a173499fe904d9ce7a9be2c113e12b801d9fc
Will Beasley <wibeasley@hotmail.com>
2021-11-03 16:51:51 -0500

    ambiguous steroid attempt to read json
    ref #8

---
+++ steroid-background/manipulation/steroid-ambiguous.R
@@ -0,0 +1,137 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+
+# Import only certain functions of a package into the search path.
+import::from("magrittr", "%>%")
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("readr"        )
+requireNamespace("tidyr"        )
+requireNamespace("dplyr"        ) # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("rlang"        ) # Language constructs, like quosures
+requireNamespace("checkmate"    ) # For asserting conditions meet expected patterns/conditions. # remotes::install_github("mllg/checkmate")
+requireNamespace("DBI"          ) # Database-agnostic interface
+requireNamespace("RSQLite"      ) # Lightweight database for non-PHI data.
+# requireNamespace("odbc"         ) # For communicating with SQL Server over a locally-configured DSN.  Uncomment if you use 'upload-to-db' chunk.
+requireNamespace("OuhscMunge"   ) # remotes::install_github(repo="OuhscBbmc/OuhscMunge")
+
+# ---- declare-globals ---------------------------------------------------------
+# Constant values that won't change.
+config                         <- config::get()
+
+# Execute to specify the column types.  It might require some manual adjustment (eg doubles to integers).
+#   OuhscMunge::readr_spec_aligned(config$path_subject_1_raw)
+# col_types <- readr::cols_only(
+#   subject_id          = readr::col_integer(),
+#   county_id           = readr::col_integer(),
+#   gender_id           = readr::col_integer(),
+#   race                = readr::col_character(),
+#   ethnicity           = readr::col_character()
+# )
+
+paths <-
+  c(
+    "concept-sets/nasal-spray.json",
+    "concept-sets/inhaled-corticosteroid.json",
+    "concept-sets/oral-dexamethasone.json",
+    "concept-sets/oral-hydrocortisone.json",
+    "concept-sets/systemic-hydrocortisone.json",
+    "concept-sets/systemic-prednisolone.json",
+    "concept-sets/systemic-prednosone-and-methyprednisolone.json"
+  )
+
+# ---- load-data ---------------------------------------------------------------
+# Read the CSVs
+paths[1] |>
+  jsonlite::read_json(simplifyVector = TRUE) |>
+  tibble::as_tibble() |>
+  dplyr::pull(items) |>
+  # purrr::flatten_dfc() |>
+  View()
+
+  dplyr::select(concept$CONCEPT_ID, isExcluded)
+  str()
+  dplyr::pull(concept) |>
+  dplyr::select(
+    CONCEPT_ID,
+
+  )
+  str()
+
+  # purrr::map_dfr(jsonlite::read_json()
+
+
+rm(col_types)
+
+# Print the first few rows of each table, especially if you're stitching with knitr (see first line of this file).
+#   If you print, make sure that the datasets don't contain any PHI.
+#   A normal `data.frame` will print all rows.  But `readr::read_csv()` returns a `tibble::tibble`,
+#   which prints only the first 10 rows by default.  It also lists the data type of each column.
+ds
+
+# ---- tweak-data --------------------------------------------------------------
+# OuhscMunge::column_rename_headstart(ds) # Help write `dplyr::select()` call.
+ds <-
+  ds %>%
+  dplyr::select(    # `dplyr::select()` drops columns not included.
+    subject_id,
+    county_id,
+    gender_id,
+    race,
+    ethnicity,
+  ) %>%
+  # dplyr::mutate(
+  # )  %>%
+  dplyr::arrange(subject_id) # %>%
+  # tibble::rowid_to_column("subject_id") # Add a unique index if necessary
+
+# ---- verify-values -----------------------------------------------------------
+# OuhscMunge::verify_value_headstart(ds)
+checkmate::assert_integer(  ds$subject_id , any.missing=F , lower=1001, upper=1200 , unique=T)
+checkmate::assert_integer(  ds$county_id  , any.missing=F , lower=1, upper=77     )
+checkmate::assert_numeric(  ds$gender_id  , any.missing=F , lower=1, upper=255     )
+checkmate::assert_character(ds$race       , any.missing=F , pattern="^.{5,41}$"    )
+checkmate::assert_character(ds$ethnicity  , any.missing=F , pattern="^.{18,30}$"   )
+
+# ---- specify-columns-to-upload -----------------------------------------------
+# Print colnames that `dplyr::select()`  should contain below:
+#   cat(paste0("    ", colnames(ds), collapse=",\n"))
+
+# Define the subset of columns that will be needed in the analyses.
+#   The fewer columns that are exported, the fewer things that can break downstream.
+
+ds_slim <-
+  ds %>%
+  # dplyr::slice(1:100) %>%
+  dplyr::select(
+    subject_id,
+    county_id,
+    gender_id,
+    race,
+    ethnicity,
+  )
+
+ds_slim
+
+# ---- save-to-disk ------------------------------------------------------------
+# If there's no PHI, a rectangular CSV is usually adequate, and it's portable to other machines and software.
+# readr::write_csv(ds_slim, path_out_unified)
+# readr::write_rds(ds_slim, path_out_unified, compress="gz") # Save as a compressed R-binary file if it's large or has a lot of factors.
+
+
+# ---- save-to-db --------------------------------------------------------------
+# If a database already exists, this single function uploads to a SQL Server database.
+# OuhscMunge::upload_sqls_odbc(
+#   d             = ds_slim,
+#   schema_name   = "skeleton",         # Or config$schema_name,
+#   table_name    = "subject",
+#   dsn_name      = "skeleton-example", # Or config$dsn_qqqqq,
+#   timezone      = config$time_zone_local, # Uncomment if uploading non-UTC datetimes
+#   clear_table   = T,
+#   create_table  = F
+# ) # 0.012 minutes
