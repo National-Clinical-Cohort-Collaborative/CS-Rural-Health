823cd9a078ac72fcf1571ec11df24a8f857fc768
Will Beasley <wibeasley@hotmail.com>
2022-01-09 15:58:54 -0600

    Update ss-steroid-create-3.sql
    ref #10

--- steroid-background/manipulation/one-timers/ss-steroid-create-3.sql
+++ steroid-background/manipulation/one-timers/ss-steroid-create-3.sql
@@ -32,14 +32,11 @@ with ingredient as (
     )
 ,downstream as (
   SELECT
-    --top 1000
-    --concept_id_1
-    --,
-    concept_id_2           as concept_id
-    --,c1.concept_name
-    --,c2.concept_name
-    --,c2.*
-    ----,*
+    cr.concept_id_2                              as concept_id
+    ,string_agg(c2.concept_name, ';')            as ingredient_names
+    ,string_agg(cr.concept_id_1, ';')            as ingredient_concept_ids
+    -- ,group_concat(c2.concept_name, ';')          as ingredient_names
+    -- ,group_concat(cr.concept_id_1, ';')          as ingredient_concept_ids
   FROM [v6].[concept_relationship]  cr
     left  join v6.concept c1 on cr.concept_id_1 = c1.concept_id
     left  join v6.concept c2 on cr.concept_id_2 = c2.concept_id
@@ -48,8 +45,8 @@ with ingredient as (
     and
     cr.relationship_id = 'Mapped from'
     and
-    --cr.concept_id_1 = cr.concept_id_2
-    --and
+    -- cr.concept_id_1 != cr.concept_id_2
+    -- and
     c1.concept_class_id = 'ingredient'
     and
     c2.vocabulary_id != 'RxNorm Extension'
@@ -57,16 +54,15 @@ with ingredient as (
     c1.concept_id in (SELECT i.concept_id FROM ingredient i)
     --and
     --c2.concept_name like '%dexameth%'
+  GROUP BY cr.concept_id_2
 )
-,descendant as (
+-- ,descendant as (
   SELECT
     ca.descendant_concept_id                      as concept_id
-    -- ,group_concat(a.concept_name, ';')            as ancestor_names
-    -- ,group_concat(ca.ancestor_concept_id, ';')    as ancestor_concept_ids
-    -- -- ,ca.min_levels_of_separation
-    -- -- ,ca.max_levels_of_separation
-    -- -- ,a.concept_id   as ancestor_id
-    -- -- ,d.concept_id   as descendent_id
+    ,string_agg(a.concept_name, ';')              as ingredient_names          -- about 100 duplicates
+    ,string_agg(ca.ancestor_concept_id, ';')      as ingredient_concept_ids
+    -- ,group_concat(a.concept_name, ';')            as ingredient_names
+    -- ,group_concat(ca.ancestor_concept_id, ';')    as ingredient_concept_ids
   FROM v6.concept_ancestor ca
     inner join v6.concept a on ca.ancestor_concept_id = a.concept_id -- stands for ancestor
     inner join v6.concept d on ca.descendant_concept_id = d.concept_id -- stands for descendant
@@ -75,18 +71,18 @@ with ingredient as (
     -- and
     d.vocabulary_id != 'RxNorm Extension'  --d.vocabulary_id = 'RxNorm'
     and
-    ca.ancestor_concept_id in (SELECT concept_id FROM ingredient)
+    ca.ancestor_concept_id in (SELECT i.concept_id FROM ingredient i)
   GROUP BY ca.descendant_concept_id
-)
+-- )
 
-SELECT
-  *
-FROM v6.concept_ancestor ca
-  left  join v6.concept c2 on ca.descendant_concept_id = c2.concept_id
-WHERE
-  ca.ancestor_concept_id in (SELECT do.concept_id FROM downstream do)
-  and
-  -- ca.descendant_concept_id not in (SELECT d.concept_id FROM descendant d)
-  -- and
-  c2.vocabulary_id != 'RxNorm Extension'
-ORDER BY ca.ancestor_concept_id, c2.concept_name
+-- SELECT
+--   *
+-- FROM v6.concept_ancestor ca
+--   left  join v6.concept c2 on ca.descendant_concept_id = c2.concept_id
+-- WHERE
+--   ca.ancestor_concept_id in (SELECT do.concept_id FROM downstream do)
+--   and
+--   -- ca.descendant_concept_id not in (SELECT d.concept_id FROM descendant d)
+--   -- and
+--   c2.vocabulary_id != 'RxNorm Extension'
+-- ORDER BY ca.ancestor_concept_id, c2.concept_name
