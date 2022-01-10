c584706496a6f3b5528cbd5c165a4d7e8caf879a
Will Beasley <wibeasley@hotmail.com>
2022-01-09 19:51:27 -0600

    stacking & collapsing
    ref #10

--- steroid-background/manipulation/codeset-unify-2.R
+++ steroid-background/manipulation/codeset-unify-2.R
@@ -25,24 +25,12 @@ config                         <- config::get()
 
 sql_retrieve <-
   "
-    with descendant as (
+    with ingredient as (
       SELECT
-        ca.descendant_concept_id                      as concept_id
-        ,group_concat(a.concept_name, ';')            as ancestor_names
-        ,group_concat(ca.ancestor_concept_id, ';')    as ancestor_concept_ids
-        -- ,ca.min_levels_of_separation
-        -- ,ca.max_levels_of_separation
-        -- ,a.concept_id   as ancestor_id
-        -- ,d.concept_id   as descendent_id
-      FROM concept_ancestor ca
-        inner join concept a on ca.ancestor_concept_id = a.concept_id -- stands for ancestor
-        inner join concept d on ca.descendant_concept_id = d.concept_id -- stands for descendant
+        concept_id
+      FROM concept
       WHERE
-        -- d.standard_concept = 'S'
-        -- and
-        d.vocabulary_id != 'RxNorm Extension'  --d.vocabulary_id = 'RxNorm'
-        and
-        ca.ancestor_concept_id in (  -- ancestor concept name
+        concept_id in (  -- ancestor concept name
           905151           -- 'alclometasone'                      -- probably ignore b/c mostly topical
           ,1115572         -- 'beclomethasone'
           ,92048           -- 'betamethasone'
@@ -67,19 +55,70 @@ sql_retrieve <-
           ,19011127        -- 'prednylidene'
           ,903963          -- 'triamcinolone'
         )
-      GROUP BY ca.descendant_concept_id
-      ORDER BY
-        a.concept_name
-        ,d.concept_name
     )
+    ,downstream as (
+      SELECT
+        cr.concept_id_2                              as concept_id
+        ,cr.concept_id_1                             as ingredient_concept_id
+      FROM concept_relationship_mapped_from  cr
+        left  join concept c1 on cr.concept_id_1 = c1.concept_id
+        left  join concept c2 on cr.concept_id_2 = c2.concept_id
+      WHERE
+        -- cr.invalid_reason is null
+        -- and
+        -- cr.relationship_id = 'Mapped from'
+        -- and
+        -- cr.concept_id_1 != cr.concept_id_2
+        -- and
+        c1.concept_class_id = 'ingredient'
+        and
+        c2.vocabulary_id != 'RxNorm Extension'
+        and
+        c1.concept_id in (SELECT i.concept_id FROM ingredient i)
+        --and
+        --c2.concept_name like '%dexameth%'
+    )
+    ,descendant as (
+      SELECT
+        ca.descendant_concept_id                      as concept_id
+        ,ca.ancestor_concept_id                       as ingredient_concept_id
+      FROM concept_ancestor ca
+        inner join concept a on ca.ancestor_concept_id = a.concept_id -- stands for ancestor
+        inner join concept d on ca.descendant_concept_id = d.concept_id -- stands for descendant
+      WHERE
+        -- d.standard_concept = 'S'
+        -- and
+        d.vocabulary_id != 'RxNorm Extension'  --d.vocabulary_id = 'RxNorm'
+        and
+        ca.ancestor_concept_id in (SELECT i.concept_id FROM ingredient i)
+    )
+    ,stack as (
+      SELECT concept_id, ingredient_concept_id FROM downstream
+      UNION
+      SELECT concept_id, ingredient_concept_id FROM descendant
+    )
+    ,collapsed as (
+      SELECT
+        s.concept_id
+        --,string_agg(s.ingredient_concept_id, ';')      as ingredient_concept_ids -- about 100 duplicates
+        --,string_agg(i.concept_name, ';')               as ingredient_names
+        ,group_concat(s.ingredient_concept_id, ';')    as ingredient_concept_ids
+        ,group_concat(i.concept_name, ';')             as ingredient_names
+        ,count(distinct s.ingredient_concept_id)       as ingredient_count
+      FROM stack s
+        inner join concept i on s.ingredient_concept_id = i.concept_id
+      GROUP BY s.concept_id
+    )
+    -- ,cte as (
     SELECT
-      d.concept_id
+      co.concept_id
       ,case
         when c.concept_name like '%drug implant%'   then 'systemic'
         when c.concept_name like '%inject%'         then 'systemic'
         when c.concept_name like '%oral%'           then 'systemic'
         when c.concept_name like '%pill%'           then 'systemic'
         when c.concept_name like '%syringe%'        then 'systemic'
+        when c.concept_name like '% systemic%'      then 'systemic'
         when c.concept_name like '%tablet%'         then 'systemic'
 
         when c.concept_name like '%inhal%'          then 'inhaled'
@@ -92,28 +131,36 @@ sql_retrieve <-
         when c.concept_name like '%ointment%'       then 'other'
         when c.concept_name like '%ophthal%'        then 'other'
         when c.concept_name like '%otic%'           then 'other'
-        when c.concept_name like '% pad %'          then 'other'
-        when c.concept_name like '% paste %'        then 'other'
+        when c.concept_name like '% pad%'           then 'other'
+        when c.concept_name like '% paste%'         then 'other'
         when c.concept_name like '%rectal%'         then 'other'
         when c.concept_name like '%shampoo%'        then 'other'
+        when c.concept_name like '% soap%'          then 'other'
+        when c.concept_name like '%spray%'          then 'other'
         when c.concept_name like '%tape%'           then 'other'
         when c.concept_name like '%toothpaste%'     then 'other'
         when c.concept_name like '%topical%'        then 'other'
         when c.concept_name like '%vaginal%'        then 'other'
       end                        as guess
+      ,co.ingredient_concept_ids
+      ,co.ingredient_names
+      ,co.ingredient_count
       ,c.concept_name
-      ,d.ancestor_names
-      ,d.ancestor_concept_ids
-      ,c.standard_concept
-      ,c.invalid_reason
-      ,c.concept_class_id
       ,c.vocabulary_id
-      ,c.concept_code
-    FROM descendant d
-      inner join concept c on d.concept_id = c.concept_id
-    ORDER BY
-      c.concept_name
-      ,d.ancestor_names
+      ,c.concept_class_id
+      ,c.standard_concept
+    FROM collapsed co
+      inner join concept c on co.concept_id = c.concept_id
+    ORDER BY ingredient_names, c.concept_name
+    -- )
+
+    -- SELECT
+    --   concept_id
+    --   ,concept_name
+    -- FROM cte
+    -- WHERE guess is null
+    -- ORDER BY concept_name
+
   "
 
 # OuhscMunge::readr_spec_aligned(config$path_steroid_classification)
--- steroid-background/manipulation/one-timers/ss-steroid-create-3.sql
+++ steroid-background/manipulation/one-timers/ss-steroid-create-3.sql
@@ -1,43 +1,39 @@
 
 with ingredient as (
-      SELECT
-        concept_id
-      FROM v6.concept
-      WHERE
-        concept_id in (  -- ancestor concept name
-          905151           -- 'alclometasone'                      -- probably ignore b/c mostly topical
-          ,1115572         -- 'beclomethasone'
-          ,92048           -- 'betamethasone'
-          ,939259          -- 'budesonide'
-          ,902938          -- 'ciclesonide'
-          ,19050907        -- 'cloprednol'
-          ,1000632         -- 'clotrimazole'                       -- probably ignore b/c mostly topical
-          ,1507705         -- 'cortisone'
-          ,19061907        -- 'cortivazol'
-          ,19086888        -- 'deflazacort'
-          ,1518254         -- 'dexamethasone'
-          ,19111234        -- 'fluprednisolone'
-          ,1149380         -- 'fluticasone'
-          ,975125          -- 'hydrocortisone'
-          ,21602737        -- 'hydrocortisone; systemic'
-          ,19009116        -- 'meprednisone'
-          ,1506270         -- 'methylprednisolone'
-          ,905233          -- 'mometasone'
-          ,19027186        -- 'paramethasone'
-          ,1550557         -- 'prednisolone'
-          ,1551099         -- 'prednisone'
-          ,19011127        -- 'prednylidene'
-          ,903963          -- 'triamcinolone'
-        )
+  SELECT
+    concept_id
+  FROM v6.concept
+  WHERE
+    concept_id in (  -- ancestor concept name
+      905151           -- 'alclometasone'                      -- probably ignore b/c mostly topical
+      ,1115572         -- 'beclomethasone'
+      ,92048           -- 'betamethasone'
+      ,939259          -- 'budesonide'
+      ,902938          -- 'ciclesonide'
+      ,19050907        -- 'cloprednol'
+      ,1000632         -- 'clotrimazole'                       -- probably ignore b/c mostly topical
+      ,1507705         -- 'cortisone'
+      ,19061907        -- 'cortivazol'
+      ,19086888        -- 'deflazacort'
+      ,1518254         -- 'dexamethasone'
+      ,19111234        -- 'fluprednisolone'
+      ,1149380         -- 'fluticasone'
+      ,975125          -- 'hydrocortisone'
+      ,21602737        -- 'hydrocortisone; systemic'
+      ,19009116        -- 'meprednisone'
+      ,1506270         -- 'methylprednisolone'
+      ,905233          -- 'mometasone'
+      ,19027186        -- 'paramethasone'
+      ,1550557         -- 'prednisolone'
+      ,1551099         -- 'prednisone'
+      ,19011127        -- 'prednylidene'
+      ,903963          -- 'triamcinolone'
     )
+)
 ,downstream as (
   SELECT
     cr.concept_id_2                              as concept_id
     ,cr.concept_id_1                             as ingredient_concept_id
-    -- ,string_agg(c2.concept_name, ';')            as ingredient_names
-    -- ,string_agg(cr.concept_id_1, ';')            as ingredient_concept_ids
-    -- ,group_concat(c2.concept_name, ';')          as ingredient_names
-    -- ,group_concat(cr.concept_id_1, ';')          as ingredient_concept_ids
   FROM [v6].[concept_relationship]  cr
     left  join v6.concept c1 on cr.concept_id_1 = c1.concept_id
     left  join v6.concept c2 on cr.concept_id_2 = c2.concept_id
@@ -55,16 +51,11 @@ with ingredient as (
     c1.concept_id in (SELECT i.concept_id FROM ingredient i)
     --and
     --c2.concept_name like '%dexameth%'
-  -- GROUP BY cr.concept_id_2
 )
 ,descendant as (
   SELECT
     ca.descendant_concept_id                      as concept_id
     ,ca.ancestor_concept_id                       as ingredient_concept_id
-    -- ,string_agg(a.concept_name, ';')              as ingredient_names          -- about 100 duplicates
-    -- ,string_agg(ca.ancestor_concept_id, ';')      as ingredient_concept_ids
-    -- ,group_concat(a.concept_name, ';')            as ingredient_names
-    -- ,group_concat(ca.ancestor_concept_id, ';')    as ingredient_concept_ids
   FROM v6.concept_ancestor ca
     inner join v6.concept a on ca.ancestor_concept_id = a.concept_id -- stands for ancestor
     inner join v6.concept d on ca.descendant_concept_id = d.concept_id -- stands for descendant
@@ -74,18 +65,11 @@ with ingredient as (
     d.vocabulary_id != 'RxNorm Extension'  --d.vocabulary_id = 'RxNorm'
     and
     ca.ancestor_concept_id in (SELECT i.concept_id FROM ingredient i)
-  -- GROUP BY ca.descendant_concept_id
 )
 ,stack as (
-  SELECT
-    concept_id
-    ,ingredient_concept_id
-  FROM downstream
+  SELECT concept_id, ingredient_concept_id FROM downstream
   UNION
-  SELECT
-    concept_id
-    ,ingredient_concept_id
-  FROM descendant
+  SELECT concept_id, ingredient_concept_id FROM descendant
 )
 ,collapsed as (
   SELECT
@@ -99,8 +83,39 @@ with ingredient as (
     inner join v6.concept i on s.ingredient_concept_id = i.concept_id
   GROUP BY s.concept_id
 )
+-- ,cte as (
 SELECT
   co.concept_id
+  ,case
+    when c.concept_name like '%drug implant%'   then 'systemic'
+    when c.concept_name like '%inject%'         then 'systemic'
+    when c.concept_name like '%oral%'           then 'systemic'
+    when c.concept_name like '%pill%'           then 'systemic'
+    when c.concept_name like '%syringe%'        then 'systemic'
+    when c.concept_name like '% systemic%'      then 'systemic'
+    when c.concept_name like '%tablet%'         then 'systemic'
+
+    when c.concept_name like '%inhal%'          then 'inhaled'
+
+    when c.concept_name like '%nasal%'          then 'nasal'
+
+    when c.concept_name like '%cream%'          then 'other'
+    when c.concept_name like '%enema%'          then 'other'
+    when c.concept_name like '%itch%'           then 'other'
+    when c.concept_name like '%ointment%'       then 'other'
+    when c.concept_name like '%ophthal%'        then 'other'
+    when c.concept_name like '%otic%'           then 'other'
+    when c.concept_name like '% pad%'           then 'other'
+    when c.concept_name like '% paste%'         then 'other'
+    when c.concept_name like '%rectal%'         then 'other'
+    when c.concept_name like '%shampoo%'        then 'other'
+    when c.concept_name like '% soap%'          then 'other'
+    when c.concept_name like '%spray%'          then 'other'
+    when c.concept_name like '%tape%'           then 'other'
+    when c.concept_name like '%toothpaste%'     then 'other'
+    when c.concept_name like '%topical%'        then 'other'
+    when c.concept_name like '%vaginal%'        then 'other'
+  end                        as guess
   ,co.ingredient_concept_ids
   ,co.ingredient_names
   ,co.ingredient_count
@@ -110,15 +125,12 @@ SELECT
   ,c.standard_concept
 FROM collapsed co
   inner join v6.concept c on co.concept_id = c.concept_id
-ORDER BY ingredient_names
+ORDER BY ingredient_names, c.concept_name
+-- )
+
 -- SELECT
---   *
--- FROM v6.concept_ancestor ca
---   left  join v6.concept c2 on ca.descendant_concept_id = c2.concept_id
--- WHERE
---   ca.ancestor_concept_id in (SELECT do.concept_id FROM downstream do)
---   and
---   -- ca.descendant_concept_id not in (SELECT d.concept_id FROM descendant d)
---   -- and
---   c2.vocabulary_id != 'RxNorm Extension'
--- ORDER BY ca.ancestor_concept_id, c2.concept_name
+--   concept_id
+--   ,concept_name
+-- FROM cte
+-- WHERE guess is null
+-- ORDER BY concept_name
