ae98c98d9c1afad217005970fd1cccb38813eda5
Will Beasley <wibeasley@hotmail.com>
2022-02-25 12:24:27 -0600

    switch to native pipe
    @jerrodanzalone, switch the pipes to the magrittr style if you run this on an old version of R
    
    ref #15

--- zip-code-characteristics/manipulation/zip-code-hospital.R
+++ zip-code-characteristics/manipulation/zip-code-hospital.R
@@ -1,4 +1,4 @@
-# knitr::stitch_rmd(script="manipulation/te-ellis.R", output="stitched-output/manipulation/te-ellis.md") # dir.create("stitched-output/manipulation/", recursive=T)
+# knitr::stitch_rmd(script="manipulation/zip-code-hospital.R", output="stitched-output/manipulation/zip-code-hospital.md") # dir.create("stitched-output/manipulation/", recursive=T)
 rm(list = ls(all.names = TRUE))  # Clear the variables from previous runs.
 
 # ---- load-sources ------------------------------------------------------------
@@ -6,7 +6,7 @@ rm(list = ls(all.names = TRUE))  # Clear the variables from previous runs.
 
 # ---- load-packages -----------------------------------------------------------
 # Attach these package(s) so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
-import::from("magrittr", "%>%")
+# import::from("magrittr", "%>%")
 
 # Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 requireNamespace("readr"        )
@@ -23,7 +23,7 @@ config    <- config::get()
 
 zip_paths <- fs::dir_ls(regexp = config$path_raw_zip_code_zcta_pattern, recurse = T, perl = T)
 
-# figure_path <- 'stitched-output/manipulation/te/'
+# figure_path <- 'stitched-output/manipulation/zip-code-hospital/'
 
 col_types_zcta <- readr::cols_only(
   GEOID           = readr::col_character(),
@@ -60,26 +60,26 @@ rm(col_types_zcta, col_types_hospital)
 # OuhscMunge::column_rename_headstart(ds_city) #Spit out columns to help write call ato `dplyr::rename()`.
 
 ds_zcta_census <-
-  ds_zcta_census %>%
-  # dplyr::slice(1:200) %>%
+  ds_zcta_census |>
+  # dplyr::slice(1:200) |>
   dplyr::select(    # `dplyr::select()` drops columns not mentioned.
     zip_code    = GEOID,
     long        = INTPTLONG,
     lat         = INTPTLAT,
     file_path
-  ) %>%
-  rematch2::bind_re_match(from = file_path, config$path_raw_zip_code_zcta_pattern) %>%
+  ) |>
+  rematch2::bind_re_match(from = file_path, config$path_raw_zip_code_zcta_pattern) |>
   dplyr::mutate(
     year  = as.integer(year),
     # zip_code    = sprintf("%05i", zip_code) # If the variable had been converted to an integer
-  ) %>%
-  tibble::as_tibble() %>%
-  dplyr::group_by(zip_code) %>%
+  ) |>
+  tibble::as_tibble() |>
+  dplyr::group_by(zip_code) |>
   dplyr::mutate(
     index_within_year   = dplyr::n() - dplyr::row_number(year),
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(index_within_year == 0L) %>%
+  ) |>
+  dplyr::ungroup() |>
+  dplyr::filter(index_within_year == 0L) |>
   dplyr::select(    # `dplyr::select()` drops columns not mentioned.
     zip_code,
     long,
@@ -88,8 +88,8 @@ ds_zcta_census <-
   )
 
 ds_hospital <-
-  ds_hospital %>%
-  # dplyr::slice(1:200) %>%
+  ds_hospital |>
+  # dplyr::slice(1:200) |>
   dplyr::select(    # `dplyr::select()` drops columns not included.
     hospital_id   = OBJECTID,
     long          = LONGITUDE,
@@ -97,13 +97,13 @@ ds_hospital <-
     status        = STATUS,
     hospital_type = TYPE,
     bed_count     = BEDS,
-  ) %>%
-  dplyr::filter(status == "OPEN") %>%
-  dplyr::filter(hospital_type %in% c("CRITICAL ACCESS", "GENERAL ACUTE CARE")) %>%
+  ) |>
+  dplyr::filter(status == "OPEN") |>
+  dplyr::filter(hospital_type %in% c("CRITICAL ACCESS", "GENERAL ACUTE CARE")) |>
   dplyr::mutate(
     hospital_type  = factor(tolower(hospital_type)),
     bed_count      = dplyr::na_if(bed_count, -999L),
-  ) %>%
+  ) |>
   dplyr::select(
     hospital_id,
     long,
@@ -137,8 +137,8 @@ ds_zcta_hospital <- # One row represents a [zip]-by-[hospital] combination
         z.lat  between h.lat  - 3 and h.lat  + 3
         and
         z.long between h.long - 4 and h.long + 4
-  " %>%
-  sqldf::sqldf() %>%
+  " |>
+  sqldf::sqldf() |>
   tibble::as_tibble()
 })
 
@@ -146,15 +146,15 @@ ds_zcta_hospital <- # One row represents a [zip]-by-[hospital] combination
 message("Distance start time: ", Sys.time())
 system.time({
 ds_zcta_hospital_2 <- # One row represents a [zip]-by-[hospital] combination
-  ds_zcta_hospital %>%
-  # dplyr::slice(1:20000) %>%
+  ds_zcta_hospital |>
+  # dplyr::slice(1:20000) |>
   dplyr::mutate(
     distance_from_zip_code_to_hospital_in_miles =
       geosphere::distVincentyEllipsoid(
         p1  = cbind(.data$z_long, .data$z_lat),
         p2  = cbind(.data$h_long, .data$h_lat)
       ) * config$miles_per_m
-  ) %>%
+  ) |>
   dplyr::mutate(
     within_20mi  = (distance_from_zip_code_to_hospital_in_miles <=  20L),
     within_60mi  = (distance_from_zip_code_to_hospital_in_miles <=  60L),
@@ -164,18 +164,18 @@ ds_zcta_hospital_2 <- # One row represents a [zip]-by-[hospital] combination
 
 ds_zcta_hospital_type <- # One row per [zip] -by- [hospital type]
   ds_zcta_hospital_2 |>
-  dplyr::group_by(zip_code, hospital_type) %>%
+  dplyr::group_by(zip_code, hospital_type) |>
   dplyr::summarize(
     distance_min      = as.integer(round(min(distance_from_zip_code_to_hospital_in_miles))),
     hospital_count_within_20mi  = sum(within_20mi ),
     hospital_count_within_60mi  = sum(within_60mi ),
     hospital_count_within_100mi = sum(within_100mi),
-  ) %>%
+  ) |>
   dplyr::ungroup()
 
 ds_zcta <-  # One row per [zip]
   ds_zcta_hospital_2 |>
-  dplyr::group_by(zip_code) %>%
+  dplyr::group_by(zip_code) |>
   dplyr::summarize(
     bed_count_within_20mi   = sum(bed_count * within_20mi , na.rm = TRUE),
     bed_count_within_60mi   = sum(bed_count * within_60mi , na.rm = TRUE),
@@ -183,9 +183,9 @@ ds_zcta <-  # One row per [zip]
   ) |>
   dplyr::ungroup()
 
-# Pivot to one row per zip,a nd join with two other zip-grain datasets.
+# Pivot to one row per zip, and join with two other zip-grain datasets.
 ds_wide <-
-  ds_zcta_hospital_type %>%
+  ds_zcta_hospital_type |>
   dplyr::mutate(
     hospital_type =
       dplyr::recode(
@@ -193,7 +193,7 @@ ds_wide <-
         "critical access"     = "critical",
         "general acute care"  = "acute"
       )
-  ) %>%
+  ) |>
   tidyr::pivot_wider(
     id_cols     = "zip_code",
     names_from  = "hospital_type",
@@ -203,7 +203,7 @@ ds_wide <-
       "hospital_count_within_60mi",
       "hospital_count_within_100mi"
     )
-  ) %>%
+  ) |>
   dplyr::mutate(
     hospital_count_within_20mi_acute        = dplyr::coalesce(hospital_count_within_20mi_acute      , 0L),
     hospital_count_within_20mi_critical     = dplyr::coalesce(hospital_count_within_20mi_critical   , 0L),
@@ -211,9 +211,9 @@ ds_wide <-
     hospital_count_within_60mi_critical     = dplyr::coalesce(hospital_count_within_60mi_critical   , 0L),
     hospital_count_within_100mi_acute       = dplyr::coalesce(hospital_count_within_100mi_acute     , 0L),
     hospital_count_within_100mi_critical    = dplyr::coalesce(hospital_count_within_100mi_critical  , 0L),
-  ) %>%
+  ) |>
   dplyr::left_join( # join with the bed counts
-    ds_zcta %>%
+    ds_zcta |>
       dplyr::select(
         zip_code,
         bed_count_within_20mi,
@@ -221,9 +221,9 @@ ds_wide <-
         bed_count_within_100mi,
       ),
     by = "zip_code"
-  ) %>%
+  ) |>
   dplyr::left_join( # join to get the last year of info for the zip
-    ds_zcta_census %>%
+    ds_zcta_census |>
       dplyr::select(
         zip_code,
         year_last_existed,
@@ -259,8 +259,8 @@ checkmate::assert_integer(  ds_wide$year_last_existed                   , any.mi
 # Define the subset of columns that will be needed in the analyses.
 #   The fewer columns that are exported, the fewer things that can break downstream.
 ds_slim <-
-  ds_wide %>%
-  # dplyr::slice(1:100) %>%
+  ds_wide |>
+  # dplyr::slice(1:100) |>
   dplyr::select(
     zip_code,
     distance_min_acute,
