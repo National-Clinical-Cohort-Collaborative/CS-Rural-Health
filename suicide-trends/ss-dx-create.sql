ab6d0910705b092561cf77c54ea5807d2818f366
Will Beasley <wibeasley@hotmail.com>
2023-07-14 14:53:11 -0500

    map icd to snomed
    ref #24

--- suicide-trends/ss-dx-create.sql
+++ suicide-trends/ss-dx-create.sql
@@ -1,13 +1,10 @@
-SELECT TOP (1000) 
-  concept_id
-  ,concat('[', concept_code, ']')   as icd_code
-  ,''                               as category
-  ,'TRUE'                           as include_in_analysis
-  ,concept_name                     as icd_description
-  ,domain_id
-  ,vocabulary_id
-  ,concept_class_id
-FROM [cdw_omop_1].[v6].[concept]
+-- use cdw_omop_1;
+WITH concept_icd10cm as (
+SELECT
+  concept_id    as icd_concept_id
+  ,concept_code as icd_code
+  ,concept_name as icd_description
+FROM v6.concept
 WHERE
   (
     vocabulary_id  = 'ICD10CM'
@@ -24,3 +21,25 @@ WHERE
     --and
     --concept_name like '%self%inf%'
   )
+)
+
+SELECT
+  ct.concept_id                     as target_concept_id
+  ,ct.domain_id                     as target_domain_id
+  ,'TRUE'                           as include_in_analysis
+  ,''                               as category
+  ,cs.icd_code
+  ,cs.icd_description
+  ,ct.concept_name                  as target_description
+  ,cs.icd_concept_id
+  ,ct.vocabulary_id                 as target_vocabulary
+FROM concept_icd10cm cs -- for *c*oncept *s*ource (icd10cm)
+  left  join v6.concept_source_to_target cst on cs.icd_concept_id = cst.source_concept_id
+  left  join v6.concept ct                   on cst.target_concept_id = ct.concept_id -- for *c*oncept *t*arget (snomed)
+  
+--WHERE 
+--  c1.concept_id in (SELECT 
+
+
+
+  --[cdw_omop_1].[v6].[concept]
